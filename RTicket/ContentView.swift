eae863ecf4d971797cd2c536cd924b713378b205
Griffin Velichko <griffin.velichko@gmail.com>
2024-08-25 10:53:00 -0700

    Initial Commit

---
+++ RTicket/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  RTicket
+//
+//  Created by Griffin Velichko on 2024-08-24.
+//
+
+//import SwiftUI
+//
+//struct ContentView: View {
+//    @State private var username = ""
+//
+//    var body: some View {
+//        NavigationView {
+//            if (username == "") {
+//                EmailLoginView(username: $username)
+//            } else {
+//                ProductView(username: username)
+//                    .toolbar {
+//                        ToolbarItem(placement: .topBarTrailing) {
+//                            LogoutButton(username: $username)
+//                        }
+//                    }
+//            }
+//        }
+//    }
+//}
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username: String = realmApp.currentUser?.profile.email ?? ""
+
+    var body: some View {
+        NavigationView {
+            if username != "" {
+                ProductView(username: username)
+                    .toolbar {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            LogoutButton(username: $username)
+                        }
+                    }
+            } else {
+                EmailLoginView(username: $username)
+            }
+        }
+    }
+}
---
+++ RTicket/RTicketApp.swift
@@ -0,0 +1,17 @@
+//
+//  RTicketApp.swift
+//  RTicket
+//
+//  Created by Griffin Velichko on 2024-08-25.
+//
+
+import SwiftUI
+
+@main
+struct RTicketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
